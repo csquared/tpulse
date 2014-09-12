b57b546bd9d7c066ad36a0484d83cf3883045ad3
Chris Continanza <christopher.continanza@gmail.com>
2014-09-12 11:54:34 -0700

    refactor but still f-ing up copying

--- main.go
+++ main.go
@@ -7,33 +7,31 @@ import (
 	"time"
 )
 
+var delay time.Duration = 125
+
+func runAndSleep(colour string) {
+	cmd := exec.Command("tmux", "set-window-option", "-gq", "status-b", colour)
+	err := cmd.Run()
+	if err != nil {
+		log.Println(err)
+	}
+	time.Sleep(delay * time.Millisecond)
+}
+
 func main() {
 	start := 232
 	i := 0
 	max := 16
-	var delay time.Duration = 125
 
 	for {
 		for ; i <= max; i++ {
-			colour := "colour" + strconv.Itoa(start+i)
-			cmd := exec.Command("tmux", "set-window-option", "-g", "status-b", colour)
-			err := cmd.Run()
-			if err != nil {
-				log.Println(err)
-			}
-			time.Sleep(delay * time.Millisecond)
+			runAndSleep("colour" + strconv.Itoa(start+i))
 		}
 
 		time.Sleep(4 * delay * time.Millisecond)
 
 		for ; i > 0; i-- {
-			colour := "colour" + strconv.Itoa(start+i)
-			cmd := exec.Command("tmux", "set-window-option", "-g", "status-b", colour)
-			err := cmd.Run()
-			if err != nil {
-				log.Println(err)
-			}
-			time.Sleep(delay * time.Millisecond)
+			runAndSleep("colour" + strconv.Itoa(start+i))
 		}
 
 		time.Sleep(4 * delay * time.Millisecond)
